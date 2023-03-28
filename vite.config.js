7803f95b134f6541890b9db539f7deed5e9f4808
karen <ana_karen_jasso@outlook.com>
2023-03-27 22:47:21 -0600

    add base in vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://anakaren29.github.io/tab-panel-project",
+  base:"https://anakaren29.github.io/tab-panel",
 })
